3f07a2ad4dc3ce40a119810c6ed1d1baf3a58adf
RAJATH <98030630+rajathshetty04@users.noreply.github.com>
2023-06-20 17:28:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,17 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
+
+app.post('/examples', (req, res) => {
+  // Create a new example
+  const example = {
+    name: req.body.name,
+    age: req.body.age,
+  };
+
+  res.json(example);
+});
+
+app.listen(process.env.PORT || 3000 , () => {
+  console.log('Server is listening on port 3000');
+});
